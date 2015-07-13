16b95a83fc5b1072c64f01e9247b90c176744015
Utsav Jain <utsavjain1408@gmail.com>
2015-07-13 19:48:05 +0530

    First Commit

---
+++ New/src/pkgnew/Info.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkgnew;
+
+/**
+ *
+ * @author Utsav
+ */
+public class Info {
+    public String Nam;
+    public String Add;
+    public String Gen;
+            
+            
+            
+    
+}
